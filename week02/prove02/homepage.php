84549b9ef01d96b58450f920673d1caa5daa2bd7
Robert Hampton <robbzilla@byui.edu>
2020-05-02 20:54:23 -0500

    Changes to homepage and index

--- week02/prove02/homepage.php
+++ week02/prove02/homepage.php
@@ -1,137 +1,134 @@
-<?php
-echo '<!DOCTYPE html>';
-echo '<html lang="en">';
-echo '<head>';
-echo '<title>02 Prove - Personal Homepage</title>';
-echo '<meta charset="utf-8">';
-echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
-echo '<link rel="stylesheet" href="style.css">';
-echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">';
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>';
-echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>';
-echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>';
-echo '</head>';
-echo '<body>';
-echo '';
-echo '<div class="jumbotron text-center">';
-echo '<h1>ĠІɅϨЅ</h1>';
-echo '<p>design.</p>';
-echo '</div>';
-echo '';
-echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">';
-echo '<!-- Links -->';
-echo '<ul class="navbar-nav mx-auto">';
-echo '<li class="nav-item">';
-echo '<a class="nav-link" href="homepage.html">Home</a>';
-echo '</li>';
-echo '<li class="nav-item">';
-echo '<a class="nav-link" href="links.html">Assignments</a>';
-echo '</li>';
-echo '<li class="nav-item">';
-echo '<a class="nav-link" href="#">Gallery</a>';
-echo '</li>';
-echo '</ul>';
-echo '</nav>';
-echo '<br>';
-echo '';
-echo '<div class="container">';
-echo '<div class="row">';
-echo '<div class="col-sm-4">';
-echo '<h3>Design & User Experience</h3>';
-echo '<p>Building beautiful layouts and intuitive interfaces that put users\'';
-echo 'needs first is more craft than art, and we take it seriously here';
-echo 'at ĠІɅϨЅ. Thoughtful, considerate design is at the heart of all we do.';
-echo 'Whether you are a startup looking to find your voice or an established';
-echo 'brand looking for a fresh perspective, our creative team is eager to';
-echo 'grab a pencil and get to work</p>';
-echo '</div>';
-echo '<div class="col-sm-4">';
-echo '<h3>Social & Content</h3>';
-echo '<p>ĠІɅϨЅ can help plan, create, and publish high-quality original content';
-echo 'on all the cool social networks, as well as a few of the less cool ones';
-echo '(looking at you, linked in). We’ll also help you amplify and measure the';
-echo 'reach and engagement and all that other fun stuff. We work with a variety';
-echo 'of photographers specializing in everything from food and events to VR,';
-echo 'up to and including VR of food being prepared at events.</p>';
-echo '</div>';
-echo '<div class="col-sm-4">';
-echo '<h3>Development</h3>';
-echo '<p>Our development team can handle everything from the latest responsive';
-echo 'front end markup frameworks to deep backend design and execution. We';
-echo 'consider ourselves technology agnostic and believe in using the right tools';
-echo 'for the job. Having said that, we do have some favorites, and those include';
-echo 'Python and Node.js, and we’re getting into Go.</p>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '';
-echo '<div id="demo" class="carousel slide" data-ride="carousel">';
-echo '';
-echo '<!-- Indicators -->';
-echo '<ul class="carousel-indicators">';
-echo '<li data-target="#demo" data-slide-to="0" class="active"></li>';
-echo '<li data-target="#demo" data-slide-to="1"></li>';
-echo '<li data-target="#demo" data-slide-to="2"></li>';
-echo '</ul>';
-echo '';
-echo '<!-- The slideshow -->';
-echo '<div class="container">';
-echo '<div class="carousel-inner">';
-echo '<div class="carousel-item active">';
-echo '<img src="sign.jpg" alt="Do Something Great" width="1100" height="500">';
-echo '</div>';
-echo '<div class="carousel-item">';
-echo '<img src="laptop.jpg" alt="Coding" width="1100" height="500">';
-echo '</div>';
-echo '<div class="carousel-item">';
-echo '<img src="code.jpg" alt="More Code" width="1100" height="500">';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '';
-echo '<!-- Left and right controls -->';
-echo '<a class="carousel-control-prev" href="#demo" data-slide="prev">';
-echo '<span class="carousel-control-prev-icon"></span>';
-echo '</a>';
-echo '<a class="carousel-control-next" href="#demo" data-slide="next">';
-echo '<span class="carousel-control-next-icon"></span>';
-echo '</a>';
-echo '</div>';
-echo '<br>';
-echo '';
-echo '<div class="container">';
-echo '<div class="row">';
-echo '<div class="col-sm-4">';
-echo '<div class="card">';
-echo '<img class="card-img-top" src="me.jpg" alt="Card image" style="width:100%">';
-echo '<div class="card-body">';
-echo '<h4 class="card-title">Robert Hampton</h4>';
-echo '<p class="card-text">Robert is an architect, engineer, and web design legend. Known to most';
-echo 'as Robzzilla, and to close friends as "Big Money Grip". Founder of ĠІɅϨЅ, Former CEO of';
-echo 'Amazon, well established circus clown that you should never turn your back to.';
-echo '</p>';
-echo '<a href="https://www.linkedin.com/in/robbzilla/" class="btn btn-primary">See Profile</a>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '<div class="col-sm-8">';
-echo '<h3>Why we\'re moving our agency, and clients, to Kubernetes</h3>';
-echo '<p>"Kubernetes represents a dramatically new way to develop and host web sites."</p>';
-echo '<a href="homepage.html" class="btn btn-primary">Read Full Article</a>';
-echo '<h3>You don\'t need a CMS</h3>';
-echo '<p>“If we\'re the only people who are going to be messing around with your CMS, you probably don\'t need to implement it”</p>';
-echo '<a href="homepage.html" class="btn btn-primary">Read Full Article</a>';
-echo '<h3>How to get a digital design interview in 30 seconds or less</h3>';
-echo '<p>“When applications are stacked 100 deep in an inbox, you only get one chance to make a good first impression.';
-echo 'The good news is it\'s not rocket science to stand out from the pack.”</p>';
-echo '<a href="homepage.html" class="btn btn-primary">Read Full Article</a>';
-echo '<h3>Creating a collaborative work space</h3>';
-echo '<p>“More specifically- finding, renovating and furnishing a physical work space to collaborate in.”</p>';
-echo '<a href="homepage.html" class="btn btn-primary">Read Full Article</a>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '</body>';
-echo '</html>';
-echo '';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>02 Prove - Personal Homepage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>ĠІɅϨЅ</h1>
+  <p>design.</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Links -->
+  <ul class="navbar-nav mx-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="homepage.html">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="links.html">Assignments</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Gallery</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Design & User Experience</h3>
+      <p>Building beautiful layouts and intuitive interfaces that put users'
+      needs first is more craft than art, and we take it seriously here
+      at ĠІɅϨЅ. Thoughtful, considerate design is at the heart of all we do.
+      Whether you are a startup looking to find your voice or an established
+      brand looking for a fresh perspective, our creative team is eager to 
+      grab a pencil and get to work</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Social & Content</h3>
+      <p>ĠІɅϨЅ can help plan, create, and publish high-quality original content 
+        on all the cool social networks, as well as a few of the less cool ones 
+        (looking at you, linked in). We’ll also help you amplify and measure the 
+        reach and engagement and all that other fun stuff. We work with a variety 
+        of photographers specializing in everything from food and events to VR, 
+        up to and including VR of food being prepared at events.</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Development</h3>        
+      <p>Our development team can handle everything from the latest responsive 
+        front end markup frameworks to deep backend design and execution. We 
+        consider ourselves technology agnostic and believe in using the right tools 
+        for the job. Having said that, we do have some favorites, and those include 
+        Python and Node.js, and we’re getting into Go.</p>
+    </div>
+  </div>
+</div>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="container">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="sign.jpg" alt="Do Something Great" width="1100" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="laptop.jpg" alt="Coding" width="1100" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="code.jpg" alt="More Code" width="1100" height="500">
+      </div>
+    </div>
+</div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<br>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card">
+        <img class="card-img-top" src="me.jpg" alt="Card image" style="width:100%">
+        <div class="card-body">
+          <h4 class="card-title">Robert Hampton</h4>
+          <p class="card-text">Robert is an architect, engineer, and web design legend. Known to most
+              as Robzzilla, and to close friends as "Big Money Grip". Founder of ĠІɅϨЅ, Former CEO of
+              Amazon, well established circus clown that you should never turn your back to.
+          </p>
+          <a href="https://www.linkedin.com/in/robbzilla/" class="btn btn-primary">See Profile</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-8">
+      <h3>Why we're moving our agency, and clients, to Kubernetes</h3>        
+      <p>"Kubernetes represents a dramatically new way to develop and host web sites."</p>
+      <a href="homepage.html" class="btn btn-primary">Read Full Article</a>
+      <h3>You don't need a CMS</h3>        
+      <p>“If we're the only people who are going to be messing around with your CMS, you probably don't need to implement it”</p>
+      <a href="homepage.html" class="btn btn-primary">Read Full Article</a>
+      <h3>How to get a digital design interview in 30 seconds or less</h3>        
+      <p>“When applications are stacked 100 deep in an inbox, you only get one chance to make a good first impression. 
+        The good news is it's not rocket science to stand out from the pack.”</p>
+      <a href="homepage.html" class="btn btn-primary">Read Full Article</a>
+      <h3>Creating a collaborative work space</h3>        
+      <p>“More specifically- finding, renovating and furnishing a physical work space to collaborate in.”</p>
+      <a href="homepage.html" class="btn btn-primary">Read Full Article</a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
