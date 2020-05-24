f317fa9796d2a13dbc3493affd77b1a59b1594ed
Robert Hampton <robbzilla@byui.edu>
2020-05-23 21:01:30 -0500

    Added changes to main in week 5

--- week05/main.php
+++ week05/main.php
@@ -1,3 +1,42 @@
 <?php
+    try
+    {
+        $dbUrl = getenv('DATABASE_URL');
 
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php';?>
+<body>
+    <h1>Movies</h1>
+    <form action="">
+        <select>
+            <?php
+            foreach ($db->query('SELECT DISTINCT name FROM movie') as $row)
+            {
+                echo '<option value="' . $row['name'] . '">' . $row['name'] . '</option>';
+            }
+            ?>
+        </select>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
